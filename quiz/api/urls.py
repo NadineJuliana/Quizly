7f26361799db7230753df14c87af76566e2ad812
NadineJuliana <nadine.juliana.b@web.de>
2026-08-18 16:20:53 +0200

    feat: add quiz API routes

--- quiz/api/urls.py
+++ quiz/api/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import QuizListCreateView, QuizDetailView
+
+
+urlpatterns = [
+    path("", QuizListCreateView.as_view(), name="quiz-list-create"),
+    path("<int:pk>/", QuizDetailView.as_view(), name="quiz-detail"),
+]
