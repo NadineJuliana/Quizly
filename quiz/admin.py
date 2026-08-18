0de3305ffdd22a0bfdd2e4c11cbf076889e40038
NadineJuliana <nadine.juliana.b@web.de>
2026-08-18 23:47:40 +0200

    feat: register quiz models in admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
 
+from quiz.models import Quiz, Question
+
 # Register your models here.
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "title",
+        "user",
+        "video_url",
+        "created_at",
+    )
+
+    search_fields = (
+        "title",
+        "user__username",
+        "video_url",
+    )
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "question_title",
+        "quiz",
+        "answer",
+        "created_at",
+    )
+
+    search_fields = (
+        "question_title",
+        "quiz__title",
+        "answer",
+    )
